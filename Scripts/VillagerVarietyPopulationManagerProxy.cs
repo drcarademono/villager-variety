b0055ebbd3c6ef0c43d23893cb1b8c2c2cf318c6
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2022-02-27 22:02:49 -0500

    Implemented climate variants. Currently supported climate variant is "S" for Subtropical and Swamp climates. Mountains in Hammerfell also use "S" for now. Mobile villagers spawned in Hammerfell are now always Redguard in appearance.

---
+++ Scripts/VillagerVarietyPopulationManagerProxy.cs
@@ -0,0 +1,40 @@
+using DaggerfallConnect.Arena2;
+using DaggerfallWorkshop;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Utility;
+
+namespace VillagerVariety
+{
+    static class VillagerVarietyPopulationManagerProxy
+    {
+        public static void Register()
+        {
+            PopulationManager.OnMobileNPCEnable += PopulationManager_OnMobileNPCEnable;
+        }
+
+        private static void PopulationManager_OnMobileNPCEnable(PopulationManager.PoolItem poolItem)
+        {
+            var PlayerGPS = GameManager.Instance.PlayerGPS;
+
+            // Daggerfall makes Swamp and Subtropical climates into Bretons using Redguard names
+            // For VV, we replace these with Redguard textures, which the MobilePerson script will replace with a Subtropical variant if available
+            if (PlayerGPS.CurrentClimateIndex == (int)MapsFile.Climates.Swamp
+                || PlayerGPS.CurrentRegionIndex == (int)MapsFile.Climates.Subtropical)
+            {
+                if (poolItem.npc.Race != Races.Redguard)
+                {
+                    poolItem.npc.RandomiseNPC(Races.Redguard);
+                }
+            }
+            else if(PlayerGPS.CurrentRegionIndex == (int)DaggerfallRegions.DragontailMountains
+                    || PlayerGPS.CurrentRegionIndex == (int)DaggerfallRegions.Ephesus)
+            {
+                if (poolItem.npc.Race != Races.Redguard)
+                {
+                    poolItem.npc.RandomiseNPC(Races.Redguard);
+                }
+            }
+        }
+    }
+}
